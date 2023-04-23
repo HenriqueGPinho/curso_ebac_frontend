c6320f4ec4fc8455c68e0de413eb902ba4a3a422
Henrique <henrique.de.nf@gmail.com>
2023-04-23 18:53:01 -0300

    add: gulp, gulp-sass, gulp-sourcemaps

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass')(require('sass'));
+
+function defaultTask() {
+  return gulp.src('./source/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build'));
+}
+
+exports.default = defaultTask;
+exports.watch = function () {
+  gulp.watch('./source/main.scss', ['sass']);
+}
