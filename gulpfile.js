e551e50f2dba6ccdca64607f9fcb52a64ef6e617
Henrique Pinho <henriquegravino@outlook.com>
2023-05-09 20:11:07 -0300

    add: gulp-imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass')(require('sass'));
+const imgmin = require('gulp-imagemin');
 
-function defaultTask() {
+function sourceMaps() {
   return gulp.src('./source/style/main.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
@@ -10,7 +11,10 @@ function defaultTask() {
     .pipe(gulp.dest('./build/style'));
 }
 
-exports.default = defaultTask;
-exports.watch = function () {
-  gulp.watch('./source/main.scss', ['sass']);
+function imagemin() {
+  return gulp.src('./source/img/*')
+    .pipe(imgmin())
+    .pipe(gulp.dest('./build/img'));
 }
+
+exports.default = gulp.parallel(sourceMaps, imagemin);
