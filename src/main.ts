6b632f91c196f9b9a95fe75a414d12623f36dc0d
Henrique Pinho <henriquegravino@outlook.com>
2024-02-24 12:20:05 -0300

    fix: function saudacao returns string

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ function produto(valor1: number, valor2: number): number {
   return valor1 * valor2;
 }
 
-function saudacao(nome: string): void {
-  console.log(`Olá, ${nome}!!`);
+function saudacao(nome: string): string {
+  return `Olá, ${nome}!!`;
 }
-
-saudacao('Henrique');
-console.log(produto(2,3));
